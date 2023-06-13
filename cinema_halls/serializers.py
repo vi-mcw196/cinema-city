de441db169c0b1cf4483e0d520599261de9368c9
vi-mcw196 <moldlova50@gmail.com>
2023-06-13 02:07:30 +0200

    update serializers.ValidationError

--- cinema_halls/serializers.py
+++ cinema_halls/serializers.py
@@ -12,15 +12,15 @@ class CinemaHallSerializer(serializers.ModelSerializer):
 
     def validate_max_seats(self, value):
         if value < MIN_SEATS or value > MAX_SEATS:
-            raise serializers.ValidationError('Max seats has to be between 1 and 10.')
+            raise serializers.ValidationError('Max seats has to be between 1 and 100.')
         return value
 
     def validate_rows(self, value):
         if value < MIN_ROWS_COLUMNS or value > MAX_ROWS_COLUMNS:
-            raise serializers.ValidationError('Rows has to be between 1 and 10.')
+            raise serializers.ValidationError('Rows has to be between 1 and 100.')
         return value
 
     def validate_columns(self, value):
         if value < MIN_ROWS_COLUMNS or value > MAX_ROWS_COLUMNS:
-            raise serializers.ValidationError('Columns has to be between 1 and 10.')
+            raise serializers.ValidationError('Columns has to be between 1 and 100.')
         return value
